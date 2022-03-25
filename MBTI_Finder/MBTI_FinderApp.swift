f313f839505509b22b635a44444aa106ba135e55
seongminkim2274 <seongmin.kim@haedenbridge.com>
2022-03-25 13:22:12 +0900

    Initial Commit

---
+++ MBTI_Finder/MBTI_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTI_FinderApp.swift
+//  MBTI_Finder
+//
+//  Created by 김성민 on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MBTI_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
